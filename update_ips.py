e6ba4925d80c49fa7870eb38b728247a9bb8bf5d
GrahLnn <grahlnn@skiff.com>
2024-07-18 13:04:19 +0800

    d

---
+++ update_ips.py
@@ -0,0 +1,15 @@
+import requests
+
+# 获取数据
+response = requests.get('https://ip.164746.xyz/ipTop.html')
+response.raise_for_status()
+data = response.text.strip()
+
+# 处理数据
+ips = data.split(',')
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+formatted_ips = [f"{ip}  #{alphabet[i % len(alphabet)]}\n" for i, ip in enumerate(ips)]
+
+# 写入文件
+with open('best_ips.txt', 'w') as f:
+    f.writelines(formatted_ips)
